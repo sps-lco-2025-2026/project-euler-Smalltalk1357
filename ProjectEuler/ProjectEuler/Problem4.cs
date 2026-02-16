07bc106dd1cb5795a4a545ef7bee0a440524d4bc
Smalltalk1357 <alex.wan@icloud.com>
2026-02-16 20:37:25 +0000

    Finished Problem4.cs

--- ProjectEuler/ProjectEuler/Problem4.cs
+++ ProjectEuler/ProjectEuler/Problem4.cs
@@ -4,9 +4,16 @@ public class Problem4
 {
     public static void Run()
     {
-        for (int i = 0; i < UPPER; i++)
+        for (int i = 999; i > 800; i--)
         {
-            
+            for (int j = 999; j > 800; j--)
+            {
+                string temp = (i * j).ToString();
+                if ((temp[0] == temp[5]) && (temp[1] == temp[4]) && (temp[2] == temp[3]))
+                {
+                    Console.WriteLine(temp);
+                }
+            }
         }
     }
 }
