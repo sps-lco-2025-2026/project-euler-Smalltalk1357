1eaab90fcb6f37359d42db63e5a3cc11bd2865c3
Smalltalk1357 <alex.wan@icloud.com>
2026-02-16 17:16:38 +0000

    Finished Problem3.cs

--- ProjectEuler/ProjectEuler/Problem3.cs
+++ ProjectEuler/ProjectEuler/Problem3.cs
@@ -4,17 +4,27 @@ public class Problem3
 {
     public static void Run()
     {
+        const long START = 600851475143;
         
+        for (long i = 1; i < START/3; i++)
+        {
+            if (START % i == 0)
+            {
+                if (IsPrime(START / i))
+                {
+                    Console.WriteLine(START / i);
+                    return;
+                }
+            }
+        }
     }
     
-    private static bool isPrime(int n)
+    private static bool IsPrime(long n)
     {
         if (n <= 1) return false;
-        int limit = n / 2;
-        
+        long limit = n / 2;
         for (int i = 2; i <= limit; i++)
             if (n % i == 0) return false;
-        
         return true;
     }
 }
